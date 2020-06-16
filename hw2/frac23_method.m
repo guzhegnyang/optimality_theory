5d732adf0696b90d6a356da25cf2809a450e2446
guzhengyang <1141374791@qq.com>
2020-06-16 23:33:54 +0800

    hw2-v2

--- hw2/frac23_method.m
+++ hw2/frac23_method.m
@@ -1,47 +1,44 @@
 %frac23_method.m
-function [a, sampling] = frac23_method(f, x, d, am, km)
+function [x, sampling] = frac23_method(f, x0, x3, km)
     %f 目标函数
-    %x 当前解
-    %d 当前方向
-    %am 当前步长最大取值
+    %x0 区间左边界
+    %x3 区间右边界
     %km 最大迭代次数
     c = 2/3;                %常数0.666
     k = 1;                  %第k次迭代
-    a = zeros(1, km);       %记录计算当前步长随迭代次数变化的所有取值
+    x = zeros(1, km);       %记录计算当前步长随迭代次数变化的所有取值
     sampling = zeros(1, km);%记录采样数随迭代次数变化的所有取值
-    a0 = 0;                 %区间左边界
-    a1 = am - c * am;       %待比较点较小值
-    a2 = c * am;            %待比较点较大值
-    a3 = am;                %区间右边界
-    f1 = f(x + a1 * d);
-    f2 = f(x + a2 * d);
+    x1 = x3 - c * (x3 - x0);%待比较点较小值
+    x2 = c * (x3 - x0) + x0;%待比较点较大值
+    f1 = f(x1);
+    f2 = f(x2);
     sampling(1, k) = 2;
 
     while k <= km - 1
         if f1 >= f2
-            a0 = a1;
-            a1 = a3 - c * (a3 - a0);
-            a2 = c * (a3 - a0) + a0;
-            a(1, k) = f2;
+            x(1, k) = x2;
             k = k + 1;
-            f1 = f(x + a1 * d);
-            f2 = f(x + a2 * d);
+            x0 = x1;
+            x1 = x3 - c * (x3 - x0);
+            x2 = c * (x3 - x0) + x0;
+            f1 = f(x1);
+            f2 = f(x2);
             sampling(1, k) = sampling(1, k - 1) + 2;
         else
-            a3 = a2;
-            a2 = c * (a3 - a0) + a0;
-            a1 = a3 - c * (a3 - a0);
-            a(1, k) = f1;
+            x(1, k) = x1;
             k = k + 1;
-            f2 = f(x + a2 * d);
-            f1 = f(x + a1 * d);
+            x3 = x2;
+            x2 = c * (x3 - x0) + x0;
+            x1 = x3 - c * (x3 - x0);
+            f2 = f(x2);
+            f1 = f(x1);
             sampling(1, k) = sampling(1, k - 1) + 2;
         end
     end
 
     if f1 >= f2
-        a(1, k) = f2;
+        x(1, k) = x2;
     else
-        a(1, k) = f1;
+        x(1, k) = x1;
     end
 end
--- hw2/hw2.m
+++ hw2/hw2.m
@@ -1,23 +1,42 @@
 %hw2.m
-[a_opt, sampling_opt] = optimization_method(@f, 0, 1, 1, 10);
-[a_frac23, sampling_frac23] = frac23_method(@f, 0, 1, 1, 10);
+km = 100;
+[x_opt, sampling_opt] = optimization_method(@f, -1, 1, km);
+[x_frac23, sampling_frac23] = frac23_method(@f, -1, 1, km);
+
+error_opt = abs(x_opt);
+error_frac23 = abs(x_frac23);
+
+log_error_opt = log(error_opt);
+log_error_frac23 = log(error_frac23);
+
+k = 1:km;
+P_log_error_opt_k = polyfit(k, log_error_opt, 1)
+P_log_error_frac23_k = polyfit(k, log_error_frac23, 1)
+P_log_error_opt_sampling = polyfit(sampling_opt, log_error_opt, 1)
+P_log_error_frac23_sampling = polyfit(sampling_frac23, log_error_frac23, 1)
+X1 = [k; ones(1, km)];
+X2 = [1:2 * km; ones(1, 2 * km)];
 
 figure;
-plot(a_opt);
+plot(log_error_opt, '.');
 hold on;
-plot(a_frac23);
+plot(log_error_frac23, '.');
+plot(P_log_error_opt_k * X1, 'Linewidth', 1.5);
+plot(P_log_error_frac23_k * X1, 'Linewidth', 1.5);
 hold off;
-legend('0.618法','0.666法');
+legend('0.618法', '0.666法', '0.618法拟合', '0.666法拟合');
 xlabel('k');
-ylabel('error');
-title('error-k图');
+ylabel('log(error)');
+title('log(error)-k图');
 
 figure;
-plot(sampling_opt, a_opt);
+plot(sampling_opt, log_error_opt, '.');
 hold on;
-plot(sampling_frac23, a_frac23);
+plot(sampling_frac23, log_error_frac23, '.');
+plot(P_log_error_opt_sampling * X2, 'Linewidth', 1.5);
+plot(P_log_error_frac23_sampling * X2, 'Linewidth', 1.5);
 hold off;
-legend('0.618法','0.666法');
+legend('0.618法', '0.666法', '0.618法拟合', '0.666法拟合');
 xlabel('sampling');
-ylabel('error');
-title('error-sampling图');
+ylabel('log(error)');
+title('log(error)-sampling图');
